54865c5904d3f928bf2a2f479c9d59b223769b1a
osayamen <mailbuddie@gmail.com>
2022-04-30 14:15:19 -0500

    add vscode devcontainer config

--- spark-svc/app.py
+++ spark-svc/app.py
@@ -3,6 +3,7 @@ from datetime import datetime, date
 from pyspark.sql import Row
 from kafka import KafkaConsumer
 import json
+import time
 
 
 KAFKA_SVC = 'kafka:9092'
@@ -21,17 +22,68 @@ TOPIC = 'home-sensehat-temperature'
 
 print("consumming data")
 consumer = KafkaConsumer(TOPIC,
-                         group_id='my-group',
+                         group_id='my-group2',
                          bootstrap_servers=KAFKA_SVC,
                          api_version=(7, 1, 0),
                          consumer_timeout_ms=1000,
                          auto_offset_reset='earliest',
                          value_deserializer=lambda m: json.loads(m.decode('ascii')))
+print(consumer.metrics())
 
+time.sleep(5)
 for message in consumer:
-    print("here")
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
     print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                          message.offset, message.key,
                                          message.value))
+
+
+
+
+# To consume latest messages and auto-commit offsets
+# consumer = KafkaConsumer(TOPIC,
+#                          group_id='my-group',
+#                          bootstrap_servers=KAFKA_SVC,
+#                          api_version=(7, 1, 0),
+#                          consumer_timeout_ms=1000,
+#                         #  auto_offset_reset='latest',
+#                         #  enable_auto_commit=False,
+#                          value_deserializer=lambda m: json.loads(m.decode('ascii'))
+#                         )
+
+
+# consumer.subscribe(TOPIC)
+# for message in consumer:
+#     print("here")
+#     # message value and key are raw bytes -- decode if necessary!
+#     # e.g., for unicode: `message.value.decode('utf-8')`
+#     print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+#                                          message.offset, message.key,
+#                                          message.value))
+
+# consumer.close()
+# # consume earliest available messages, don't commit offsets
+# KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False)
+
+# # consume json messages
+# KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii')))
+
+# # consume msgpack
+# KafkaConsumer(value_deserializer=msgpack.unpackb)
+
+# # StopIteration if no message after 1sec
+# KafkaConsumer(consumer_timeout_ms=1000)
+
+# # Subscribe to a regex topic pattern
+# consumer = KafkaConsumer()
+# consumer.subscribe(pattern='^awesome.*')
+
+# # Use multiple consumers in parallel w/ 0.9 kafka brokers
+# # typically you would run each on a different server / process / CPU
+# consumer1 = KafkaConsumer('my-topic',
+#                           group_id='my-group',
+#                           bootstrap_servers='my.server.com')
+# consumer2 = KafkaConsumer('my-topic',
+#                           group_id='my-group',
+#                           bootstrap_servers='my.server.com')
--- twitter-producer-svc/app.py
+++ twitter-producer-svc/app.py
@@ -1,6 +1,9 @@
 
 from kafka import KafkaProducer, KafkaConsumer
 import json
+import random
+import time
+
 
 KAFKA_SVC = 'kafka:9092'
 TOPIC = 'home-sensehat-temperature'
@@ -14,51 +17,12 @@ def on_send_error(excp):
     print('Error --->', exc_info=excp)
 
 
-for i in range(10):
-    producer.send(TOPIC,{"timestamp": "2020-08-12 23:31:19.102347", "temperature": i}
+text = "a,b,c,d,e,f".split(",")
+while True:
+
+    producer.send(TOPIC,{random.choice(text): "2020-08-12 23:31:19.102347", "temperature":random.choice(text) }
                   ).add_callback(on_send_success).add_errback(on_send_error)
+    time.sleep(1)
 
-# # To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer(TOPIC,
-                         group_id='my-group',
-                         bootstrap_servers=KAFKA_SVC,
-                         api_version=(7, 1, 0),
-                         consumer_timeout_ms=1000,
-                        #  auto_offset_reset='latest',
-                        #  enable_auto_commit=False,
-                         value_deserializer=lambda m: json.loads(m.decode('ascii'))
-                        )
-consumer.subscribe(TOPIC)
-for message in consumer:
-    print("here")
-    # message value and key are raw bytes -- decode if necessary!
-    # e.g., for unicode: `message.value.decode('utf-8')`
-    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                         message.offset, message.key,
-                                         message.value))
 
-consumer.close()
-# # consume earliest available messages, don't commit offsets
-# KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False)
-
-# # consume json messages
-# KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii')))
-
-# # consume msgpack
-# KafkaConsumer(value_deserializer=msgpack.unpackb)
-
-# # StopIteration if no message after 1sec
-# KafkaConsumer(consumer_timeout_ms=1000)
-
-# # Subscribe to a regex topic pattern
-# consumer = KafkaConsumer()
-# consumer.subscribe(pattern='^awesome.*')
-
-# # Use multiple consumers in parallel w/ 0.9 kafka brokers
-# # typically you would run each on a different server / process / CPU
-# consumer1 = KafkaConsumer('my-topic',
-#                           group_id='my-group',
-#                           bootstrap_servers='my.server.com')
-# consumer2 = KafkaConsumer('my-topic',
-#                           group_id='my-group',
-#                           bootstrap_servers='my.server.com')
+producer.flush()
